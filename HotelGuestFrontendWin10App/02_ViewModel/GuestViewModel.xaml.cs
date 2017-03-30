688510895ada7e8efc79d74b3f23520451ddbd8f
Søren Roed Schack <soer622t@edu.easj.dk>
2017-03-30 14:01:39 +0200

    Tilføjet en Blank Page til hhv. 01_View- og 02_ViewModel-mapperne.

--- HotelGuestFrontendWin10App/02_ViewModel/GuestViewModel.xaml.cs
+++ HotelGuestFrontendWin10App/02_ViewModel/GuestViewModel.xaml.cs
@@ -13,16 +13,16 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
-namespace HotelGuestFrontendWin10App
+namespace HotelGuestFrontendWin10App._02_ViewModel
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class GuestViewModel : Page
     {
-        public MainPage()
+        public GuestViewModel()
         {
             this.InitializeComponent();
         }
