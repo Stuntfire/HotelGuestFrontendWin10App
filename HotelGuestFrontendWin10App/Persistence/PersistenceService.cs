94f3042b3366ab0fe5b4898e32d66bc0d18c74ce
Søren Roed Schack <soer622t@edu.easj.dk>
2017-03-30 12:54:09 +0200

    oprettet en server-url til vores WebService.

--- HotelGuestFrontendWin10App/Persistence/PersistenceService.cs
+++ HotelGuestFrontendWin10App/Persistence/PersistenceService.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using HotelGuestFrontendWin10App._03_Model;
+using System.Collections.ObjectModel;
 
 namespace HotelGuestFrontendWin10App.Persistence
 {
     class PersistenceService
     {
+        const string serverUrl = "http://hotelguestwebservice20170329095006.azurewebsites.net/";
+        public static ObservableCollection<Guest> GetAsyncGuests()
+        {
+
+        }
     }
 }
