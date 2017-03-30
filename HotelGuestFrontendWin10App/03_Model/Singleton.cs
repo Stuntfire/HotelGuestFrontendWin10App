cb9d3d2f7493ec185e86b9ca4821d08712589267
Søren Roed Schack <soer622t@edu.easj.dk>
2017-03-30 11:25:02 +0200

    Tilføjet en Singleton til 03_Model mappen.

---
+++ HotelGuestFrontendWin10App/03_Model/Singleton.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelGuestFrontendWin10App._03_Model
+{
+    public class Singleton
+    {
+        private static Singleton instance;
+
+        private Singleton() { }
+
+        public static Singleton Instance {
+            get {
+                if (Instance == null)
+                {
+                    instance = new Singleton();
+                }
+                return instance;
+            }
+        }
+
+    }
+}
