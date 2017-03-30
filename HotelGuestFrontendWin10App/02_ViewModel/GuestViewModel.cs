4a2315a08e5e8018e0672b4d5938f1d532f97c65
Søren Roed Schack <soer622t@edu.easj.dk>
2017-03-30 14:24:00 +0200

    Tilføjet en GuestList full-property der refererer til ObservableCollection<Guest> GuestCollection i Singleton-klassen.

--- HotelGuestFrontendWin10App/02_ViewModel/GuestViewModel.cs
+++ HotelGuestFrontendWin10App/02_ViewModel/GuestViewModel.cs
@@ -1,12 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using HotelGuestFrontendWin10App._03_Model;
 
 namespace HotelGuestFrontendWin10App._02_ViewModel
 {
     class GuestViewModel
     {
+        public int Guest_No { get; set; }
+
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+
+        private ObservableCollection<Guest> guestList;
+
+        public ObservableCollection<Guest> GuestList {
+            get { return guestList; }
+            set { guestList = value; }
+        }
+
+        public GuestViewModel()
+        {
+            GuestList = Singleton.Instance.GuestsCollection;
+        }
+
     }
 }
