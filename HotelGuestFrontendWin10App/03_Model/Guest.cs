45783ec5a7e7786f6cec64d9336665202d270eb7
Søren Roed Schack <soer622t@edu.easj.dk>
2017-03-30 10:47:30 +0200

    Tilføjet en Guest-class med 3 properties der har samme navn som stemmer overens med properties i Guest.cs i Hotel_Guest_Webservice.

---
+++ HotelGuestFrontendWin10App/03_Model/Guest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelGuestFrontendWin10App._03_Model
+{
+    class Guest
+    {
+        public int Guest_No { get; set; }
+
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+
+        public Guest(int guest_no, string name, string address)
+        {
+            this.Guest_No = guest_no;
+            this.Name = name;
+            this.Address = address;
+        }
+    }
+}
