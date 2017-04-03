853b5f36816143bc9e4c158c5df520fba728f39d
Søren Roed Schack <soer622t@edu.easj.dk>
2017-04-03 13:52:29 +0200

    Lavet en Common-mappe inkl. en ReæayCommand-klasse med en standard implementering af ICommand-interfacet.

---
+++ HotelGuestFrontendWin10App/Common/RelayCommand.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace HotelGuestFrontendWin10App.Common
+{
+    public class RelayCommand : ICommand
+    {
+        private readonly Action execute;
+
+        public RelayCommand(Action execute)
+        {
+            this.execute = execute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            this.execute();
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+    }
+}
