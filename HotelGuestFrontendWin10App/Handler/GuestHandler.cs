0deb3ec057f6744ad85cee154699dfe599641d15
Søren Roed Schack <soer622t@edu.easj.dk>
2017-04-03 14:06:48 +0200

    Oprettet en Handler-mappe inkl. en GuestHandler, der har en CreateGuest-metode. Denne metode opretter en ny Guest og tilføjer den GuestCollection via metoden PostGuest i Singleton-klassen.

---
+++ HotelGuestFrontendWin10App/Handler/GuestHandler.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HotelGuestFrontendWin10App._02_ViewModel;
+using HotelGuestFrontendWin10App._03_Model;
+
+namespace HotelGuestFrontendWin10App.Handler
+{
+    class GuestHandler
+    {
+        private GuestViewModel Gvm { get; set; }
+
+        public GuestHandler(GuestViewModel gvm)
+        {
+            this.Gvm = gvm;
+        }
+
+        public void CreateGuest()
+        {
+            Guest tempGuest = new Guest(Gvm.Guest_No, Gvm.Name, Gvm.Address);
+            tempGuest.Guest_No = Gvm.Guest_No;
+            tempGuest.Name = Gvm.Name;
+            tempGuest.Address = Gvm.Address;
+            Singleton.Instance.PostGuest(tempGuest);
+        }
+    }
+}
