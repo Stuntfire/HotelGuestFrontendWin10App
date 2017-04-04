9290ef5b20e95e57e52a5af8b352672de5df1bf0
Søren Roed Schack <soer622t@edu.easj.dk>
2017-04-04 20:17:37 +0200

    Tilføjet ny klasse til Model-mappen: GuestNameAndNoOfBookings.cs skal bruges når PersistenceService skal skal hente indholdet af  GuestNameAndNoOfBookings-View'et

---
+++ HotelGuestFrontendWin10App/03_Model/GuestNameAndNoOfBookings.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelGuestFrontendWin10App._03_Model
+{
+    class GuestNameAndNoOfBookings
+    {
+        public int Guest_No { get; set; }
+
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+    }
+}
